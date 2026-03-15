9030420929f727c355074060c3714a149c11161a
Taewan Park <swimingtw@gmail.com>
2026-03-16 00:18:46 +0900

    Reformat code

--- app/src/main/kotlin/dev/chungjungsoo/truetime/ui/TimeScreen.kt
+++ app/src/main/kotlin/dev/chungjungsoo/truetime/ui/TimeScreen.kt
@@ -10,9 +10,9 @@ import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import dev.chungjungsoo.truetime.R
